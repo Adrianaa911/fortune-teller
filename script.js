4103e1736963ee09e2e440fb886d6a8e4cc7715a
Adrianaa911 <150440061+Adrianaa911@users.noreply.github.com>
2023-12-27 21:02:25 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const options = [
+'It is certain',
+'It is decidedly so',
+'Without any doubt',
+'Yes, indeed',
+'You may rely on it',
+'As I see it, Yes',
+'Most likely',
+'Outlook good',
+'Yes',
+'My reply is No',
+'Do not count on it',
+'Cannot predict now',
+'My sources say No',
+'Very doubtful',
+'Concentrate and ask again',
+'Better not tell me now',
+'Signs point to Yes',
+'Outlook not so good',
+'Of course, No'
+]
+
+const magicBall = document.getElementById("mb-container");
+const response = document.getElementById("response");
+
+magicBall.addEventListener('click', function(){
+
+  const index = Math.floor(Math.random()*19);
+const message = options[index]
+magicBall.className='magicBall_out animate'
+setTimeout(()=> {
+  response.textContent = message;
+  response.style.fontSize = '1.1rem'
+  magicBall.className= 'magicBall_out'
+}, 3000)
+})
